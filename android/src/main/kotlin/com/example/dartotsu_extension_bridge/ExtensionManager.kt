cbd347671cc49ad6736994512948e2b15790f2ea
aayush262 <aayushthakur262006@gmail.com>
2025-07-09 19:43:24 +0530

    test

--- android/src/main/kotlin/com/example/dartotsu_extension_bridge/ExtensionManager.kt
+++ android/src/main/kotlin/com/example/dartotsu_extension_bridge/ExtensionManager.kt
@@ -2,17 +2,15 @@ package com.example.dartotsu_extension_bridge
 
 import android.content.Context
 import eu.kanade.tachiyomi.animesource.AnimeSource
+import eu.kanade.tachiyomi.extension.anime.model.AnimeExtension
 import eu.kanade.tachiyomi.extension.anime.model.AnimeLoadResult
 import eu.kanade.tachiyomi.extension.util.ExtensionLoader
 
 class ExtensionManager(var context: Context) {
 
-    fun getInstalledExtensions(): List<AnimeSource>? {
+    fun getInstalledExtensions(): List<AnimeExtension>? {
         var sources = ExtensionLoader.loadAnimeExtensions(context)
         var installedSource = sources.filterIsInstance<AnimeLoadResult.Success>().map { it.extension }
-            .filterIsInstance<AnimeLoadResult.Success>()
-            .map { it.extension.sources }
-            .flatten()
         return installedSource
     }
 
